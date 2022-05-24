cd2b4659ef3a657c879ca6867cf1b7dcedccef23
amatos <amatos201@gmail.com>
2022-05-24 12:07:37 -0400

    add func

--- robot-gladiators/assets/js/game.js
+++ robot-gladiators/assets/js/game.js
@@ -2,6 +2,7 @@ window.prompt("What is your robot's name?");
 
 
 // this creates a function named "fight"
-function fight(); {
+function fight() {
     window.alert("The fight(); has begun!");
   }
+  fight();
