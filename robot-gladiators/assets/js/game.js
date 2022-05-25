c1a93646a13faa82547acee19eb30596334fbee6
amatos <amatos201@gmail.com>
2022-05-25 17:38:20 -0400

    add enemy comb

--- robot-gladiators/assets/js/game.js
+++ robot-gladiators/assets/js/game.js
@@ -2,14 +2,33 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+console.log(enemyNames);
+
+console.log(enemyNames.length);
+
+var fight = function(enemyName) {
+   // fight function statements
+}
+
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+
+
+for(var i = 0; i < 3; i++) {
+  console.log("apple", i);
+}
 var fight = function() {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
